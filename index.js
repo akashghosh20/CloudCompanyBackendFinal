d9366ff1e2efbb20de878d412e08504afa47a9cd
Akash Ghosh <ag1229222@gmail,com>
2024-09-17 15:23:30 +0600

    a

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
